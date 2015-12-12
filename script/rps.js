5109da6c6f3b0dcd8e5bdbb16321bf53269d2ca1
tammerg <tg385@scarletmail.rutgers.edu>
2015-12-12 12:14:28 -0500

    Got the match results to console log. Adding modals to display results

--- script/rps.js
+++ script/rps.js
@@ -1,3 +1,4 @@
+
 var userScore =  0;
 var oppScore = 0;
 var roundCount = 1;
@@ -5,8 +6,30 @@ var computerChoice = ["Rock", "Paper", "Scissors"];
 var playerChoice;
 
 
-function gameLogic() {
-
+function gameLogic(playerChoice, oppChoice) {
+  if(playerChoice === oppChoice){
+    console.log("You got a tie!");
+} else if (playerChoice === "Rock"){
+  if(oppChoice === "Paper"){
+    console.log("You Lose :(");
+  }else {
+    console.log("You Win");
+  }
+}
+  else if (playerChoice === "Paper"){
+    if(oppChoice === "Rock"){
+      console.log("You Win :)");
+  }else {
+    console.log("You Lose!");
+  }
+}
+  else if (playerChoice === "Scissors"){
+    if(oppChoice === "Paper"){
+      console.log("You Win :)");
+  }else {
+    console.log("You Lose!");
+  }
+}
 } 
 $(document).ready(function() {
 
@@ -14,8 +37,7 @@ $(document).ready(function() {
     playerChoice = $(this).attr('data-choice');
     var randomIndex = Math.floor(Math.random()*computerChoice.length); 
     var oppChoice = computerChoice[randomIndex];
-    gameLogic(playerChoice, computerChoice);
-    console.log('Player: '+playerChoice);
-    console.log('Computer: '+oppChoice);
+    gameLogic(playerChoice, oppChoice);
+    
   });
 });

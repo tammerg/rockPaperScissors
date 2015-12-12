1ac965463b98d9e719229020718adc046e869960
Tammer Galal <tg385@scarletmail.rutgers.edu>
2015-12-11 19:36:08 -0500

    console logged player choice and computer choice moving onto conditional logic

--- script/rps.js
+++ script/rps.js
@@ -4,11 +4,22 @@ var roundCount = 1;
 var computerChoice = ["Rock", "Paper", "Scissors"];
 var playerChoice;
 
+function gameLogic(playerChoice, computerChoice){
+  if(playerChoice === "Rock" && computerChoice === 0){
+    console.log("draw!");
+  }else if (playerChoice === "Rock" && computerChoice === 1){
+    console.log("You Lose :(");
+  }else (playerChoice === "Rock" && computerChoice === 2)
+    console.log("You Lose :(");
+}
+
 $(document).ready(function() {
 
   $(".gameButtons").on("click", function(){
     playerChoice = $(this).attr('data-choice');
-    console.log(playerChoice);  
+    var randomIndex = Math.floor(Math.random()*computerChoice.length); 
+    var oppChoice = computerChoice[randomIndex];
+    gameLogic(playerChoice, computerChoice);
+  
   });
-  var
 });

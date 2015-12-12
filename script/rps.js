18416a2b680e165559382fbb5a6815d816ed3af3
Tammer Galal <tg385@scarletmail.rutgers.edu>
2015-12-11 20:22:14 -0500

    working on game logic

--- script/rps.js
+++ script/rps.js
@@ -3,22 +3,16 @@ var oppScore = 0;
 var roundCount = 1;
 var computerChoice = ["Rock", "Paper", "Scissors"];
 var playerChoice;
+var randomIndex = Math.floor(Math.random()*computerChoice.length); 
+var oppChoice = computerChoice[randomIndex];
 
 function gameLogic(playerChoice, computerChoice){
-  if(playerChoice === "Rock" && computerChoice === 0){
-    console.log("draw!");
-  }else if (playerChoice === "Rock" && computerChoice === 1){
-    console.log("You Lose :(");
-  }else (playerChoice === "Rock" && computerChoice === 2)
-    console.log("You Lose :(");
+  console.log(playerChoice, oppChoice);
 }
-
 $(document).ready(function() {
 
   $(".gameButtons").on("click", function(){
     playerChoice = $(this).attr('data-choice');
-    var randomIndex = Math.floor(Math.random()*computerChoice.length); 
-    var oppChoice = computerChoice[randomIndex];
     gameLogic(playerChoice, computerChoice);
   
   });
